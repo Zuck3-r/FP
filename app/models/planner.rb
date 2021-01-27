a3896c85346d5b1ae18ad6fcfd44d3345a96e639
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-01-27 17:11:42 +0900

    add validation

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -1,2 +1,9 @@
 class Planner < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :email, { presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, length: { maximum: 255 } }
+  validates :password, presence: true, length: { minimum: 8 }
+
+  has_secure_password
 end
--- db/migrate/20210127080029_create_planners.rb
+++ db/migrate/20210127080029_create_planners.rb
@@ -2,8 +2,8 @@ class CreatePlanners < ActiveRecord::Migration[6.1]
   def change
     create_table :planners do |t|
       t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
 
       t.timestamps
     end
