4118c03c3521a3e7666810e799db2ce3c8dcd09d
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-01-27 17:54:38 +0900

    fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 40 }
-  validates :email, { presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, length: { maximum: 255 } }
+  validates :email, { presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }, length: { maximum: 255 } }
   validates :password, presence: true, length: { minimum: 8 }
 
   has_secure_password
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe Customer, type: :model do
 
   describe 'validations' do
 
-    context 'presence' do
+    describe 'presence' do
       it { is_expected.to validate_presence_of(:name) }
       it { is_expected.to validate_presence_of(:email) }
       it { is_expected.to validate_presence_of(:password) }
     end
 
-    context 'length' do
+    describe 'length' do
       it { is_expected.to validate_length_of(:name).is_at_most(40) }
       it { is_expected.to validate_length_of(:email).is_at_most(255) }
       it { is_expected.to validate_length_of(:password).is_at_least(8) }
     end
 
-    context 'email characters' do
+    describe 'email characters' do
       it do
         is_expected.to allow_values('first.last@foo.jp',
                                     'user@example.com',
@@ -33,7 +33,7 @@ RSpec.describe Customer, type: :model do
       end
     end
 
-    context 'validate unqueness of email' do
+    describe 'validate unqueness of email' do
       let!(:customer) { create(:customer, email: 'original@example.com') }
       it 'is invalid with a duplicate email' do
         customer = build(:customer, email: 'original@example.com')
