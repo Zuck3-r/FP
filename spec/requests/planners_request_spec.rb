5a50a4c36813486d350cd9e7b47c311fc29ea4b9
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-02-03 14:38:25 +0900

    値が不正だった場合のテストを追加'

--- spec/requests/planners_request_spec.rb
+++ spec/requests/planners_request_spec.rb
@@ -18,6 +18,15 @@ RSpec.describe PlannersController, type: :request do
         is_expected.to eq 302
         expect(flash[:success]).to eq '登録完了　ログインしてください'
       end
+      context 'param is not correct' do
+        before do
+          params[:planner] = FactoryBot.attributes_for(:planner, email: '')
+        end
+        it do
+        expect{subject}.not_to change(Planner, :count)
+        is_expected.to eq 200
+        end
+      end
     end
   end
 end
