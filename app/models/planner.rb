1887d54b977f5625eb4ba2108f267ffa4acc30df
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-02-04 13:26:15 +0900

    モデルの修正と、必要なデータの追加

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -1,6 +1,7 @@
 class Planner < ApplicationRecord
   has_many :skills
   before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 40 }
   validates :email, { presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }, length: { maximum: 255 } }
   validates :password, presence: true, length: { minimum: 8 }
