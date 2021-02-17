39ad5ca47e80b01b16e9fde6e7675f3940159070
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-02-18 05:18:40 +0900

    静的なページと、ルーティングの追加

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def choose; end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'planners#new'
+  get  'static_pages/home' => 'static_pages#home'
+  get  'static_pages/choose' => 'static_pages#choose'
+  root 'static_pages#home'
   resources :planners, only: %i[new create show edit update destroy]
   resources :customers, only: %i[new create show edit update destroy]
 end
