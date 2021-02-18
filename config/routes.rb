4cd67cc5ba7eb987bc5d64464f1724165eaf66e3
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-02-18 15:24:43 +0900

    指摘箇所の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get  'static_pages/home' => 'static_pages#home'
   get  'static_pages/choose' => 'static_pages#choose'
   root 'static_pages#home'
   resources :planners, only: %i[new create show edit update destroy]
