0c6535e8457dacb499e77ff2935807801418aa64
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-02-19 15:52:43 +0900

    add login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login', to: 'sessions#create'
   get  'static_pages/choose' => 'static_pages#choose'
   root 'static_pages#home'
   resources :planners, only: %i[new create show edit update destroy]
