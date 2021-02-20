1d31e0f1f54d3730d5ef943461fa419bda11256b
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-02-21 03:51:20 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   get  'static_pages/choose' => 'static_pages#choose'
   root 'static_pages#home'
   resources :planners, only: %i[new create show edit update destroy]
