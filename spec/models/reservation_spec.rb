8b07adb80c7e503f2ff6a1f6e321e6de413a61bf
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-03-09 14:35:56 +0900

    check custom vaildation

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -7,6 +7,27 @@ RSpec.describe Reservation, type: :model do
       it { is_expected.to validate_presence_of(:date) }
       it { is_expected.to validate_presence_of(:planner_id) }
     end
+
+    describe 'custom validate' do
+
+      it 'is Saturday and invalid time table id' do
+        reservation = FactoryBot.build(:reservation, date: '2021-05-29', time_table_id: '1')
+        reservation.valid?
+        expect(reservation.errors[:time_table_id]).to include('土曜日は11時～15時までしか選択できません')
+      end
+
+      it 'Sunday' do
+        reservation = FactoryBot.build(:reservation, date: '2021-05-30')
+        reservation.valid?
+        expect(reservation.errors[:date]).to include('日曜日は働いちゃダメです！')
+      end
+
+      it 'today or past day' do
+        reservation = FactoryBot.build(:reservation, date: Date.today.strftime)
+        reservation.valid?
+        expect(reservation.errors[:date]).to include('明日以降の日付を登録して下さい')
+      end
+    end
   end
 
   describe 'Association' do
