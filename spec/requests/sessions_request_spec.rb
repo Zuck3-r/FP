70431d3678f9970da8efc193a473ead35497f791
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-03-10 16:55:05 +0900

    プランナー、カスタマーのログインテスト成功

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
+require 'rspec/request_describer'
 
 RSpec.describe "Sessions", type: :request do
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/sessions/new"
-      expect(response).to have_http_status(:success)
+  describe 'GET /login' do
+    it { is_expected.to eq 200 }
+  end
+
+  describe 'planner' do
+    let(:planner){ FactoryBot.create(:planner) }
+
+    it 'success login' do
+      post login_url, params: { session: { email: planner.email, password: planner.password, flag: 1 } }
+      expect(response).to redirect_to planner
+      expect(flash[:info]).to eq 'ログインしました'
+    end
+  end
+
+  describe 'customer' do
+    let(:customer){ FactoryBot.create(:customer) }
+
+    it 'success login' do
+      post login_url, params: { session: { email: customer.email, password: customer.password, flag: 0 } }
+      expect(response).to redirect_to customer
+      expect(flash[:info]).to eq 'ログインしました'
     end
   end
 end
